7a90f2992e45fc98d4596dcf847f1fbb102cb82d
GitHub Action <action@github.com>
2022-11-07 10:54:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/yochyo/intellijangularinlinejumper/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/yochyo/intellijangularinlinejumper/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yochyo.intellijangularinlinejumper.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yochyo.intellijangularinlinejumper.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
